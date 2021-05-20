1df501a5dee9c0cb98cd12197d68438906aa47e5
david bassey <davidosky1@gmail.com>
2021-05-20 23:04:42 +0100

    Test registration functionality of Api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
     user = User
-            .find_by(email: params["user"]["email"])
-            .try(:authenticate, params["user"]["password"])
+           .find_by(email: params['user']['email'])
+           .try(:authenticate, params['user']['password'])
 
     if user
       session[:user_id] = user.id
@@ -15,3 +15,4 @@ class SessionsController < ApplicationController
       render json: { status: 401 }
     end
   end
+end
